026839032fe914799888f7732028a3227d04605a
NVBS SIJANTHA <m20000706009@student.cinec.edu>
2024-02-01 14:30:08 +0530

    DeranaHEM

---
+++ firebase-init.js
@@ -0,0 +1,12 @@
+// Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAD6hE4tHPgGaqBmFQli-r1q1e-l3dtIVU",
+    authDomain: "deranahem.firebaseapp.com",
+    projectId: "deranahem",
+    storageBucket: "deranahem.appspot.com",
+    messagingSenderId: "506176836655",
+    appId: "1:506176836655:web:f03ba85189bd822670a2da"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
